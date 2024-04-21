a6ce8b644804c8f107459dd337e8c4232c23805d
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-04-20 23:14:26 -0400

    Adding Socket Message Handler

--- bun/src/services/socket/socket.pubsub.ts
+++ bun/src/services/socket/socket.pubsub.ts
@@ -1,18 +1,35 @@
+import { ServerWebSocket } from 'bun'
+import { ElysiaWS } from 'elysia/ws'
 import logger from '../../config/logger'
 
 export interface IPubSubService {
   publish<T>(channel: string, message: T): Promise<void>
   subscribe<T>(channel: string, callback: (message: T) => void): Promise<void>
 }
+export type IWebSocketMessage = {
+  type: 'subscribe' | 'unsubscribe'
+  channel: string
+}
 
-export class PubSubService implements IPubSubService {
-  constructor() {}
+export interface IWebSocketMessageHandler {
+  message(ws: ElysiaWS<ServerWebSocket<any>, any>, message: IWebSocketMessage): void
+}
 
+export class PubSubService implements IPubSubService, IWebSocketMessageHandler {
   async publish<T>(channel: string, message: T): Promise<void> {
     logger.info(`PubSubService: ${channel} - ${message}`)
   }
 
   async subscribe<T>(channel: string, callback: (message: T) => void): Promise<void> {
     logger.info(`PubSubService: ${channel} - ${callback}`)
+  }
+
+  message(ws: ElysiaWS<ServerWebSocket<any>>, message: IWebSocketMessage): void {
+    logger.info(`PubSubService: ${message.type} - ${message.channel}`)
+    if (message.type === 'subscribe') {
+      ws.subscribe(message.channel)
+    } else if (message.type === 'unsubscribe') {
+      ws.unsubscribe(message.channel)
+    }
   }
 }
