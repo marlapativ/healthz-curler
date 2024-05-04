b4a3ef6150325e25f21a7593e8c3874c3ab20243
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-05-04 14:49:49 -0400

    GO: Influx Setup

--- go/pkg/data/timeseries/influx_datasource.go
+++ go/pkg/data/timeseries/influx_datasource.go
@@ -1,12 +1,45 @@
 package datasource
 
-import "github.com/marlpativ/healthz-curler/pkg/data"
+import (
+	"log"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/marlpativ/healthz-curler/pkg/data"
+	"github.com/marlpativ/healthz-curler/pkg/env"
+)
 
 type influxDataSource struct {
+	defaultTags map[string]string
+	client      influxdb2.Client
+	writeAPI    api.WriteAPI
+	queryAPI    api.QueryAPI
 }
 
 func NewInfluxDataSource() data.TimeSeriesDataSource {
-	return &influxDataSource{}
+	url := env.GetOrDefault("INFLUX_URL", "http://localhost:8086")
+	token := env.GetOrDefault("INFLUX_TOKEN", "")
+	if token == "" {
+		log.Fatalf("INFLUX_TOKEN environment variable is required")
+	}
+	client := influxdb2.NewClient(url, token)
+
+	org := env.GetOrDefault("INFLUX_ORG", "healthz-curler")
+	bucket := env.GetOrDefault("INFLUX_BUCKET", "healthz-curler")
+
+	defaultTags := map[string]string{
+		"implementation": "go",
+		"language":       "go",
+	}
+
+	writeAPI := client.WriteAPI(org, bucket)
+	queryAPI := client.QueryAPI(org)
+	return &influxDataSource{
+		defaultTags: defaultTags,
+		client:      client,
+		writeAPI:    writeAPI,
+		queryAPI:    queryAPI,
+	}
 }
 
 func (r *influxDataSource) WritePoint(point data.TimeSeriesData) error {
