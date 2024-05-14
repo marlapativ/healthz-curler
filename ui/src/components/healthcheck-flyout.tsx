d581218b99ba152b46ea576ad0391d298007fd17
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-05-14 19:17:48 -0400

    UI: Health check flyout updates

--- ui/src/components/healthcheck-flyout.tsx
+++ ui/src/components/healthcheck-flyout.tsx
@@ -10,7 +10,10 @@ import {
   DialogTrigger
 } from '@/components/ui/dialog'
 import { HealthCheck } from '../types/healthcheck'
-import { Label } from '@radix-ui/react-label'
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage, Form } from './ui/form'
+import { useForm } from 'react-hook-form'
+import { Input } from './ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
 
 type HealthCheckFlyoutProps = {
   healthcheck?: HealthCheck
@@ -20,19 +23,13 @@ type HealthCheckFlyoutProps = {
 }
 
 export function HealthCheckFlyout({ children, open, onOpenChange, healthcheck }: HealthCheckFlyoutProps) {
-  const [current, setCurrent] = React.useState<HealthCheck | undefined>(healthcheck)
+  const form = useForm<HealthCheck>({
+    values: healthcheck,
+    mode: 'onChange'
+  })
 
   function save() {
     onOpenChange?.(false)
-    setCurrent((prev) => {
-      if (prev) {
-        return {
-          ...prev,
-          name: 'Updated Health Check'
-        }
-      }
-      return { name: 'New Health Check', url: '', protocol: 'http', port: 80, id: '1', interval: 10 }
-    })
   }
 
   return (
@@ -50,14 +47,51 @@ export function HealthCheckFlyout({ children, open, onOpenChange, healthcheck }:
               {healthcheck ? 'Update ' : 'Add '} health check with your config. Click save when you're done.
             </DialogDescription>
           </DialogHeader>
-          <div className="p-2 flex items-center justify-center">
-            <Label>{current?.name}</Label>
+          <div className="py-2 flex items-center justify-center">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(save)} className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormDescription>Health check name</FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Active</FormLabel>
+                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select active status" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="active">Active</SelectItem>
+                          <SelectItem value="inactive">Inactive</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormDescription>Select active status </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </form>
+            </Form>
           </div>
-          <DialogFooter>
-            <Button type="submit" onClick={() => save()}>
-              Save changes
-            </Button>
-          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
