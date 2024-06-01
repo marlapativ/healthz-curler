d6d4da1d81c57942415d01febfcf4670d94bb557
Marlapati Venkata Naga Sai Teja[marlapativ] <marlapati.v@northeastern.edu>
2024-05-31 23:49:14 -0400

    UI: Default Faker to point to bun server

--- ui/src/lib/env-utils.ts
+++ ui/src/lib/env-utils.ts
@@ -25,74 +25,18 @@ const fakeResponse: Record<string, unknown> = {
           port: '4206'
         }
       ]
-    },
-    {
-      id: 'node-express',
-      runtime: 'node',
-      apiVersion: 'v1',
-      server: {
-        port: '4215',
-        framework: 'Express'
-      },
-      websocket: [
-        {
-          name: 'node',
-          path: '/ws',
-          port: '4215'
-        },
-        {
-          name: 'node-socket.io',
-          path: '/socket.io',
-          port: '4216'
-        }
-      ]
-    },
-    {
-      id: 'go-fiber',
-      runtime: 'go',
-      apiVersion: 'v1',
-      server: {
-        port: '4225',
-        framework: 'fiber'
-      },
-      websocket: [
-        {
-          name: 'go',
-          path: '/ws',
-          port: '4225'
-        }
-      ]
     }
-  ],
-  '/api/v1/healthcheck': [
-    {
-      id: '1',
-      name: 'Backend',
-      description: 'Backend health check',
-      url: 'http://localhost:8080/api/v1/health',
-      interval: 5000,
-      active: true
-    },
-    {
-      id: '2',
-      name: 'Frontend',
-      description: 'Frontend health check',
-      url: 'http://localhost:8081/api/v2/health',
-      interval: 5000,
-      active: false
-    }
-  ],
-  '/api/v1/healthgraph': {}
+  ]
 }
 
 export function fetchApi(path: string, init?: RequestInit): Promise<Response> {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    if (path) {
+    if (path && fakeResponse[path]) {
       return new Promise((resolve) => {
         setTimeout(() => resolve(new Response(JSON.stringify(fakeResponse[path]))), 1000)
       })
     }
-    return Promise.resolve(new Response())
+    return fetch(`http://localhost:4205${path}`, init)
   } else {
     return fetch(getApiUrl(path), init)
   }
